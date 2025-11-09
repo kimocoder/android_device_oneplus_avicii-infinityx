fddd0ebb76711ec65ae1266dc29fddaa7af99975
Michael Bestas <mkbestas@lineageos.org>
2025-11-09 06:57:50 +0000

    avicii: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -434,7 +434,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # Touch
 PRODUCT_PACKAGES += \
